70a1b301687b432daeabe392ddcc2121db332b5a
nazim3232 <chowdhurynazim32@gmail.com>
2025-05-04 21:19:10 +0600

    This is the Weather App .

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let desh = "Bangladesh"
+
+function allert()
+{
+    alert(`Hello Mother-Fucker and this is the another country called the ${desh}.`);
+}
+
+
+let btn = document.querySelector('button');
+btn.addEventListener("click", allert);
